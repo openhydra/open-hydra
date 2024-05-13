a9ca1ed505382d6914bde62d42fd4e9dccdb41a5
yangchenjun <yang.chenjun@99cloud.net>
2024-05-13 09:39:00 +0800

    add sandbox schema

--- pkg/open-hydra/apis/api.go
+++ pkg/open-hydra/apis/api.go
@@ -11,3 +11,16 @@ type GpuSet struct {
 	GpuDriverName string `json:"gpu_driver_name"`
 	Gpu           uint8  `json:"gpu"`
 }
+
+type Sandbox struct {
+	CPUImageName    string   `json:"cpuImageName"`
+	GPUImageName    string   `json:"gpuImageName"`
+	Command         []string `json:"command"`
+	Description     string   `json:"description"`
+	DevelopmentInfo []string `json:"developmentInfo"`
+	Status          string   `json:"status"`
+}
+
+type SandboxList struct {
+	Sandboxes map[string]Sandbox `json:"sandboxes"`
+}
