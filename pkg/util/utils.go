869491b18f68d5cb11806c79cc8ed56c047a7694
yangchenjun <yang.chenjun@99cloud.net>
2024-11-19 11:10:47 +0800

    always copy readme.md to user workspace

--- pkg/util/utils.go
+++ pkg/util/utils.go
@@ -132,3 +132,24 @@ func GetStringValueOrDefault(targetDescription, targetValue, defaultValue string
 	}
 	return defaultValue
 }
+
+func CopyFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
